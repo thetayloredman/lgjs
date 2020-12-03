1fb541644466cc7b2138023a3caa70e41c2a50fc
thetayloredman <nutdriver716@gmail.com>
2020-12-03 12:04:23 -0800

    Remove return type

--- src/logger/LogFile.ts
+++ src/logger/LogFile.ts
@@ -43,7 +43,6 @@ export default class LogFile {
      * Makes the base directory for the LogFile
      * @private
      * @function
-     * @returns {undefined}
      */
     private _init(): void {
         ensureDir(this.baseDir).then(() => {
