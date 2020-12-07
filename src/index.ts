cdbf6808f7170317a9c215efb1e73952efae689a
thetayloredman <nutdriver716@gmail.com>
2020-12-06 19:52:53 -0800

    how do i screw up so bad

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /**
  * lgjs | A Node.js log framework
- * Copyright (C) <year>  <name of author>
+ * Copyright (C) 2020  BadBoyHaloCat
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
